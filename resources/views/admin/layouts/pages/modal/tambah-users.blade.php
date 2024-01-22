1ea0b9551927ca983e0d7571f9eb1d514d97b048
MrZwen <gedeagusadityadharma@gmail.com>
2024-01-22 14:13:03 +0800

    Users CRUD

--- resources/views/admin/layouts/pages/modal/tambah-users.blade.php
+++ resources/views/admin/layouts/pages/modal/tambah-users.blade.php
@@ -20,16 +20,21 @@
                 @csrf
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
-                        <label for="tipe_kamar" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipe Kamar</label>
-                        <input type="text" name="tipe_kamar" id="tipe_kamar" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Masukkan Tipe Kamar " required="" value="{{old('tipe_kamar')}}">
+                        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                        <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Masukkan Tipe Kamar " required="" value="{{old('username')}}">
                     </div>
                     <div class="col-span-2 sm:col-span-1">
                         <label for="no_kamar" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">No Kamar</label>
                         <input type="number" name="no_kamar" id="no_kamar" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Masukkan No Kamar " required="" value="{{old('no_kamar')}}">
                     </div>
                     <div class="col-span-2 sm:col-span-1">
-                        <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga</label>
-                        <input type="number" name="harga" id="harga" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Masukkan Harga Kamar " required="" value="{{old('harga')}}">
+                        <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                        <select id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option selected="">Select Role</option>
+                            @foreach($data as $item)
+                            <option value="{{$item->role}}">{{$item->role}}</option>
+                            @endforeach
+                        </select>
                     </div>
                 </div>
                 <button type="submit" class="text-white inline-flex items-center bg-purple-600 hover:opacity-85 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
